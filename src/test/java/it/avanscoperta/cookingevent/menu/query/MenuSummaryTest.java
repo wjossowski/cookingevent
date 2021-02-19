2edb86453f83043af4855f64a524f35de75b40db
Alberto Brandolini <alberto.brandolini@gmail.com>
2021-02-19 12:34:52 +0100

    Projection e test (minimale)

--- src/test/java/it/avanscoperta/cookingevent/menu/query/MenuSummaryTest.java
+++ src/test/java/it/avanscoperta/cookingevent/menu/query/MenuSummaryTest.java
@@ -2,24 +2,19 @@ package it.avanscoperta.cookingevent.menu.query;
 
 import it.avanscoperta.cookingevent.menu.builders.MenuBuilder;
 import it.avanscoperta.cookingevent.menu.domain.MenuCreato;
-import it.avanscoperta.cookingevent.menu.query.MenuSummaryProjection;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class MenuSummaryTest {
 
-
     @Autowired
     MenuSummaryProjection projection;
-
 
     @Test
     public void can_save_a_menu() {
@@ -28,8 +23,6 @@ public class MenuSummaryTest {
         projection.on(menuCreato);
 
         Optional<MenuSummaryView> menuSummaryView = projection.find(menuCreato.getMenuId().toString());
-
         assertTrue(menuSummaryView.isPresent());
-
     }
 }
